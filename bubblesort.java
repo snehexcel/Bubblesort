6fda9c180810986b39979862520ce12dabf6741f
Sneha Kumari <snehabaranwal5613@gmail.com>
2024-03-18 00:20:15 +0530

    Add files via upload

---
+++ bubblesort.java
@@ -0,0 +1,36 @@
+public class bubblesort {
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Unsorted array:");
+        printArray(array);
+
+        bubbleSort(array);
+
+        System.out.println("\nSorted array:");
+        printArray(array);
+    }
+
+    // Bubble sort function
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    // swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    // Utility function to print an array
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
